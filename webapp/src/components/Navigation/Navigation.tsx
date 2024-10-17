d92e45b88926ccb14e9d28c676a4f0549ca7466c
Arnoldo <arnoldoaqv@gmail.com>
2024-10-16 21:52:10 -0600

    playing-around-with-stuff

--- webapp/src/components/Navigation/Navigation.tsx
+++ webapp/src/components/Navigation/Navigation.tsx
@@ -10,21 +10,14 @@ import { SortBy } from '../../modules/routing/types'
 import { VendorName } from '../../modules/vendor'
 import { Section } from '../../modules/vendor/decentraland'
 import * as decentraland from '../../modules/vendor/decentraland'
-import * as events from '../../utils/events'
 import { AssetStatusFilter } from '../../utils/filters'
-import { CAMPAING_TAB_ANIMATION_ENABLED } from '../Campaign/config'
 import { Props, NavigationTab } from './Navigation.types'
 import './Navigation.css'
 
 const Navigation = (props: Props) => {
-  const { activeTab, isFullscreen, isCampaignBrowserEnabled, onOpenBuyManaWithFiatModal, onClearFilters } = props
-  const analytics = getAnalytics()
-  const isMobile = useMobileMediaQuery()
+  const { activeTab, isFullscreen, onClearFilters } = props
+ 
 
-  const handleOpenBuyManaWithFiatModal = () => {
-    analytics.track(events.OPEN_BUY_MANA_MODAL)
-    onOpenBuyManaWithFiatModal()
-  }
 
   const browseDefaultOptions = {
     section: decentraland.Section.WEARABLES,
@@ -38,39 +31,8 @@ const Navigation = (props: Props) => {
     <div className="Navigation">
       <Tabs isFullscreen={isFullscreen}>
         <Tabs.Left>
-          <Link to={locations.root()}>
-            <Tabs.Tab active={activeTab === NavigationTab.OVERVIEW}>{t('navigation.overview')}</Tabs.Tab>
-          </Link>
-          {isCampaignBrowserEnabled ? (
-            <Link
-              to={locations.campaign({
-                section: decentraland.Section.WEARABLES,
-                vendor: VendorName.DECENTRALAND,
-                page: 1,
-                sortBy: SortBy.RECENTLY_LISTED,
-                onlyOnSale: true,
-                assetType: AssetType.ITEM
-              })}
-            >
-              <Tabs.Tab active={activeTab === NavigationTab.CAMPAIGN_BROWSER}>
-                <div
-                  className={classNames('campaign-tab', {
-                    'campaign-tab-animation': CAMPAING_TAB_ANIMATION_ENABLED
-                  })}
-                >
-                  <span>{t('campaign.tab')}</span>
-                </div>
-              </Tabs.Tab>
-            </Link>
-          ) : null}
-          <Link to={locations.browse(browseDefaultOptions)} onClick={onClearFilters}>
-            <Tabs.Tab active={activeTab === NavigationTab.COLLECTIBLES}>{t('navigation.collectibles')}</Tabs.Tab>
-          </Link>
           <Link to={locations.lands({ section: Section.LAND, assetType: AssetType.NFT })}>
             <Tabs.Tab active={activeTab === NavigationTab.LANDS}>{t('navigation.land')}</Tabs.Tab>
-          </Link>
-          <Link to={locations.claimName()}>
-            <Tabs.Tab active={activeTab === NavigationTab.NAMES}>{t('navigation.names')}</Tabs.Tab>
           </Link>
           <Link to={locations.defaultCurrentAccount()}>
             <Tabs.Tab active={activeTab === NavigationTab.MY_STORE}>{t('navigation.my_assets')}</Tabs.Tab>
@@ -84,13 +46,6 @@ const Navigation = (props: Props) => {
             </Link>
           </Mobile>
         </Tabs.Left>
-        {!isMobile ? (
-          <Tabs.Right>
-            <Button inverted onClick={handleOpenBuyManaWithFiatModal} size="small">
-              {t('navigation.buy_mana_with_fiat')}
-            </Button>
-          </Tabs.Right>
-        ) : null}
       </Tabs>
     </div>
   )
